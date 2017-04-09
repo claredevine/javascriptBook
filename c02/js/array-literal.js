92ebeda2a043ef2daf730720a93080e02c43a2d9
clare.devine <clare.devine@sky.uk>
2017-04-09 17:21:30 +0100

    example from chapter two

---
+++ c02/js/array-literal.js
@@ -0,0 +1,15 @@
+var colors;
+colors = ['white', 'black', 'custom'];
+
+var el = document.getElementById('colors');
+el.textContent = colors[0];
+
+
+var colorsTwo = new Array (
+  'white',
+  'black',
+  'custom'
+);
+
+var elTwo = document.getElementsById('colorsTwo');
+elTwo.textContent = colors[0];
