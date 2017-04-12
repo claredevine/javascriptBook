7b78029e218b62fcd2b07d897a4e4362d7a6bb71
clare.devine <clare.devine@sky.uk>
2017-04-12 13:42:03 +0100

    chapter 3

---
+++ c03/js/basic-function.js
@@ -0,0 +1,8 @@
+var msg = 'Sign up to receive our newletter for 10% off!';
+
+function updateMessage() {
+  val el = document.getElementById('message');
+  el.textContent = msg;
+}
+
+updateMessage();
